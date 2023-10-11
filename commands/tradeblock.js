a6c24ec85556478ffa56dad712b8ac553eb8821e
DrunkenTeddy <jasphill52@gmail.com>
2023-10-11 08:49:21 -0700

    Removed trade block, added ignore for csv folder

--- commands/tradeblock.js
+++
@@ -1,7 +0,0 @@
-const { ContextMenuCommandBuilder, ApplicationCommandType } = require("discord.js");
-
-module.exports = {
-  data: new ContextMenuCommandBuilder()
-    .setName("Show Trade Block")
-    .setType(ApplicationCommandType.User),
-};
