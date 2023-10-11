40ef6715253878827e24af5ef3409353d3d1476e
DrunkenTeddy <jasphill52@gmail.com>
2023-10-10 17:07:12 -0700

    Removed Draft and Trade Block Features

--- helpers/tradeBlockRemoveUntouchable.js
+++
@@ -1,9 +0,0 @@
-const query = require("./query");
-
-module.exports = async (TeamID, PlayerID) => {
-  const result = await query(`
-    DELETE FROM trade_block WHERE TeamID=${TeamID} AND PlayerID=${PlayerID};
-  `);
-
-  return result;
-};
