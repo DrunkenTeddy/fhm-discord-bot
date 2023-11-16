04c238a6be7362f56d9f1572a1b73ec4070f47a8
Jason Phillips <jasphill52@gmail.com>
2023-11-15 16:25:33 -0800

    Update getTeamFileStatus.js
    Fixed directory for team files

--- helpers/getTeamFileStatus.js
+++ helpers/getTeamFileStatus.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = (teamID) => {
   const teamFile     = `team_${teamID}.fhm`;
-  const teamFilePath = `./teamfiles/${teamFile}`;
+  const teamFilePath = `../teamfiles/${teamFile}`;
 
   if (fs.existsSync(teamFilePath)) {
     return true;
